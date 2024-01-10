0eca39ffb53a6dd9ce0dd7e1d3f59116b9971210
MartinMollo <116777040+MartinMollo@users.noreply.github.com>
2024-01-10 16:18:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ class respuesta {
 let adivinaletra
 let correcto
 let solucion
+//variables del DOM
+let texto = document.getElementById("texto")
+let palabratexto = document.getElementById("palabra")
+let informacion = document.getElementById("informacion")
+let formulario = document.getElementById("formulario")
+let fusuario = document.getElementById("fusuario")
 //creamos el array de respuestas
 const respuestas = []
 respuestas.push(new respuesta("auto", "a", "u", "t", "o"))
@@ -20,74 +26,101 @@ respuestas.push(new respuesta("caro", "c", "a", "r", "o"))
 respuestas.push(new respuesta("cruz", "c", "r", "u", "z"))
 respuestas.push(new respuesta("pelo", "p", "e", "l", "o"))
 respuestas.push(new respuesta("ruta", "r", "u", "t", "a"))
-
-while (true) {
-    let intentos = 6
-    let fallos = []
-    let letras = []
-    let letrasincognita = ["_", "_", "_", "_"]
-    // elegimos una palabra al azar
-    let numpalabra = Math.floor(Math.random() * 6)
-    //guardamos las letras en un array
-    letras.push(respuestas[numpalabra].letra1)
-    letras.push(respuestas[numpalabra].letra2)
-    letras.push(respuestas[numpalabra].letra3)
-    letras.push(respuestas[numpalabra].letra4)
-    //informacion para verificar si el programa funciona
-    console.log(respuestas[numpalabra].palabra)
-    // informo al jugador y le pido una letra y se convierte en minuscula si el usuario ingresa una mayuscula
-    adivinaletra = prompt("bienvenido al juego del ahorcado, ingrese una letra para adivinar\n" + letrasincognita + "   intentos: " + intentos + " | letras que no estan: " + fallos).toLocaleLowerCase()
-    //llamo a la funcion verificar
-    adivinaletra = verificar(adivinaletra, letrasincognita, intentos, fallos)
-    //verifico si la letra esta en la palabra
-    ubicacion = buscar(adivinaletra, letras)
-    if (ubicacion != -1) {
-        letrasincognita[ubicacion] = adivinaletra
-        correcto = true
-    } else {
-        correcto = false
-        fallos.push(adivinaletra)
-        intentos--
-    }
-    while (intentos > 0 && (letrasincognita.includes("_")) == true) {
-        //informo al jugador si la letra esta en la palabra
-        if (correcto) {
-            adivinaletra = prompt(adivinaletra + " esta en la palabra, ingrese otra letra\n" + letrasincognita + "   intentos: " + intentos + " | letras que no estan: " + fallos).toLocaleLowerCase()
-            //llamo a la funcion verificar
-            adivinaletra = verificar(adivinaletra, letrasincognita, intentos, fallos)
-        } else {
-            adivinaletra = prompt(adivinaletra + " no esta en la palabra, ingrese otra letra\n" + letrasincognita + "   intentos: " + intentos + " | letras que no estan: " + fallos).toLocaleLowerCase()
-            //llamo a la funcion verificar
-            adivinaletra = verificar(adivinaletra, letrasincognita, intentos, fallos)
-        }
-        //verifico si la letra esta en la palabra
-        ubicacion = buscar(adivinaletra, letras)
-        if (ubicacion != -1) {
-            letrasincognita[ubicacion] = adivinaletra
-            correcto = true
-        } else {
-            correcto = false
-            fallos.push(adivinaletra)
-            intentos--
-        }
-    }
-    //vemos si el jugador gano el juego
-    if (intentos != 0) {
-        alert(solucion = "felicidades, ganaste el juego con " + intentos + " intentos restantes.\n la palabra era: " + respuestas[numpalabra].palabra)
-    } else {
-        alert(solucion = "que lastima, perdiste el juego te quedaste sin intentos. \n la palabra era: " + respuestas[numpalabra].palabra)
-    }
-
+// guardo las palabras y letras en localstorage
+const rtajson = JSON.stringify(respuestas)
+localStorage.setItem("respuestas", rtajson)
+let terminar = true
+let intentos = 6
+let fallos = []
+let letras = []
+let letrasincognita = ["_", "_", "_", "_"]
+let usuario
+//si hay usuario en local lo guardo si no sera "nuevo usuario"
+const usuarioenls = localStorage.getItem("usuario")
+if (usuarioenls != null) {
+    usuario = usuarioenls
+} else {
+    usuario = "nuevo usuario"
 }
-//funciones
-function verificar(adivinaletra, letrasincognita, intentos, fallos) {
-    // el usuario no puede ingresar ni numeros ni simbolos ni palabras
-    while (adivinaletra.length > 1 || (("a" > adivinaletra || adivinaletra > "z") && adivinaletra != "ñ") || fallos.indexOf(adivinaletra) != -1) {
-        adivinaletra = prompt("jugador ingrese UNA SOLA letra que ya no haya ingresado para adivinar\n" + "     " + letrasincognita + "   intentos: " + intentos + " | letras que no estan: " + fallos).toLocaleLowerCase()
+//formulario del nombre del usuario
+fusuario.addEventListener("submit", (e) => {
+    e.preventDefault()
+    fusuario = e.target
+    usuario = fusuario.children[0].value
+    localStorage.setItem("usuario", usuario)
+    texto.innerText = "bienvenido " + usuario + "\n ingrese una letra para adivinar"
+})
+// elegimos una palabra al azar
+let numpalabra = Math.floor(Math.random() * 6)
+//guardamos las letras en un array
+letras.push(respuestas[numpalabra].letra1)
+letras.push(respuestas[numpalabra].letra2)
+letras.push(respuestas[numpalabra].letra3)
+letras.push(respuestas[numpalabra].letra4)
+//informacion para verificar si el programa funciona
+console.log(respuestas[numpalabra].palabra)
+// informo al jugador por el DOM
+texto.innerText = "bienvenido " + usuario + "\n ingrese una letra para adivinar"
+palabratexto.innerText = letrasincognita
+informacion.innerText = "   intentos: " + intentos + " | letras que no estan: " + fallos
+//formulario de la letra a ingresar
+formulario.addEventListener("submit", (e) => {
+    if (terminar) {
+        e.preventDefault()
+        formulario = e.target
+        //no quiero que el usuario ingrese una letra ya existente
+        if (adivinaletra == formulario.children[0].value || fallos.indexOf(formulario.children[0].value) != -1) {
+            texto.innerText = usuario + " ingrese otra letra por favor"
+        } else {
+            adivinaletra = formulario.children[0].value
+            //verifico si la letra esta en la palabra
+            ubicacion = buscar(adivinaletra, letras)
+            console.log(ubicacion)
+            if (ubicacion != -1) {
+                letrasincognita[ubicacion] = adivinaletra
+                correcto = true
+            } else {
+                correcto = false
+                fallos.push(adivinaletra)
+                intentos--
+            }
+            //informo al jugador si la letra esta en la palabra
+            console.log(intentos)
+            if (correcto) {
+                texto.innerText = adivinaletra + " esta en la palabra, " + usuario + "ingrese otra letra"
+                palabratexto.innerText = letrasincognita
+                informacion.innerText = "   intentos: " + intentos + " | letras que no estan: " + fallos
+            } else {
+                texto.innerText = adivinaletra + " no esta en la palabra, " + usuario + " ingrese otra letra"
+                palabratexto.innerText = letrasincognita
+                informacion.innerText = "   intentos: " + intentos + " | letras que no estan: " + fallos
+            }
+        }
+        //vemos si el jugador gano el juego
+        if (intentos == 0 || (letrasincognita.includes("_")) == false) {
+            if (intentos != 0) {
+                texto.innerText = "felicidades " + usuario + ", ganaste el juego.\n pulse aceptar para volver a jugar"
+                palabratexto.innerText = "la palabra era: " + respuestas[numpalabra].palabra
+                informacion.innerText = "   intentos: " + intentos + " | letras que no estan: " + fallos
+                terminar = false
+            } else {
+                texto.innerText = "que lastima " + usuario + ", perdiste el juego te quedaste sin intentos.\n pulse aceptar para volver a jugar"
+                palabratexto.innerText = "la palabra era: " + respuestas[numpalabra].palabra
+                informacion.innerText = "   intentos: " + intentos + " | letras que no estan: " + fallos
+                terminar = false
+            }
+        }
+    } else {
+        window.location.reload();
     }
-    return adivinaletra
-}
+})
+//limpia el localstorage
+document.getElementById("borrar").addEventListener("click", () => {
+    localStorage.clear()
+})
+//funcion
 function buscar(adivinaletra, letras) {
     let ubicacion = letras.indexOf(adivinaletra)
+    console.log(adivinaletra)
     return ubicacion
 }
